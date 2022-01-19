d790d368e71bde44f5b2db189f83fc4bbd904ec0
Sahil Maheshwari <sahilm1711@gmail.com>
2022-01-19 18:14:38 +0530

    sds
    sd

--- apps/webextension/src/app/app.tsx
+++ apps/webextension/src/app/app.tsx
@@ -1,12 +1,12 @@
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+// import styles from './app.module.css';
+// import { Sidebar } from '@coding-challenge/ui';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="webextension" />
-      <div />
-    </>
+    <div className="flex justify-center items-center h-screen w-screen">
+      {/* Replace the below div with your sidebar component from @coding-challenge/ui! */}
+      <div className="bg-blue-500">Replace Me!</div>
+    </div>
   );
 }
 
